66b3b0076e3e0550b28a5e928105c36a811dc5eb
Vo Ngo Van Tien <vongovantien@gmail.com>
2022-01-03 09:21:32 +0700

    update

--- careerproject/urls.py
+++ careerproject/urls.py
@@ -10,7 +10,6 @@ admin.site.index_title = "CareerApp"
 
 urlpatterns = [
     path('admin/', admin_site.urls),
-    # path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('careerapp.urls')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
